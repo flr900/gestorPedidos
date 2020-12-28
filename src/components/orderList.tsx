3ea298589e778d064ba826c4451599665aab9e96
flr900 <feliperodrigues9000@gmail.com>
2020-12-28 12:53:29 -0300

    docs: new version

--- src/components/orderList.tsx
+++ src/components/orderList.tsx
@@ -1,24 +1,38 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 
 import {arrayList} from "../assets/suport"
 
 import {DivContainer, ListItem} from "../styles/orderList"
 
 
+
 export default function OrderList (){
+	const [itemList,setItemList] = useState(arrayList)
+  
+	function handleStatus(id:string){
+		const newItemList = itemList.map( item => {
+			  	if(item.id == id) {
+		   		id.status = "Finalizado"
+	   	  }
+		  }
+		)
+		setItemList(newItemList)
+		
+	}
+
 	return(
 		<DivContainer>
 			<div className="wrapper">
-				{arrayList.map(item =>{
+				{itemList.map(item =>{
 					return(
-						<ListItem key={item.id}>
+						<ListItem onClick={handleStatus(item.id)}  key={item.id}>
 							<div className="itemIdentifier">
 								<h3>{item.orderTime}</h3>
 								<strong>{item.customer}</strong>
 							</div>
 							<div className="itemDetails">
 								<h3>{item.paymentMethod}</h3>
-								<h3>{item.total}</h3>
+								<h3>{ new Intl.NumberFormat("pt-BR", { style: "currency", currency: "BRL" }).format(item.total)}</h3>
 								<h3>{item.status}</h3>
 							</div>
 						</ListItem>
